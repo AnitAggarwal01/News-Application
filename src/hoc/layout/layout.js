e899c5cd83e8e62b3aaa9c885df66ef1535a7c9a
Anit Aggarwal <aaaplatinum19@gmail.com>
2019-02-16 13:44:44 +0530

    ilesa dded

--- src/hoc/layout/layout.js
+++ src/hoc/layout/layout.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
+
+import Header from '../../component/header/header';
+import Footer from '../../component/footer/footer';
 import './layout.css';
 class Layout extends Component{
     state={
-
+        showNav:false
+    }
+    toggleSideNav = (value)=>{
+        this.setState({
+            showNav:value
+        })
     }
     render= ()=>{
         return(
             <div>
-                Header
+                <Header
+                    showNav = {this.state.showNav}
+                    onHideNav ={()=>this.toggleSideNav(false)}
+                    onOpenNav ={()=>this.toggleSideNav(true)} 
+                />
                 {this.props.children}
-                Footer
+                <Footer/>
             </div>
         )
     }
